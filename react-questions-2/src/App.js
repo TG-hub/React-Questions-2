2d1908e0645c9de69c190b1c6664135b88cc2f48
Theo Goss <theo@exorttech.co.uk>
2020-08-21 10:55:19 +0100

    answer question 2 Greeter

--- react-questions-2/src/App.js
+++ react-questions-2/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter.js'
+import Greeter from './Components/Greeter.js'
 
 function App() {
   return (
     <>
       <Counter />
+      <Greeter />
     </>
   );
 }
---
+++ react-questions-2/src/Components/Greeter.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import '../App.css'
+
+export default Greeter
+
+function Input (props) {
+   
+    return ( 
+        <form>
+            <input onChange={props.textSetter} placeholder="Enter your name here"/>
+        </form>   
+    )
+}
+
+
+function Display (props) {
+    return(
+        <div>
+            <p>Hello {props.message}, good to see you!</p>
+        </div>
+    )
+}
+
+
+
+function Greeter () {
+    const [input, setInput] = useState()
+    const setText = (event) =>  setInput(event.target.value)
+    
+    return (
+    <div>
+        <Input textSetter={setText} />
+        <Display message={input} />
+    </div>
+  )  
+}
