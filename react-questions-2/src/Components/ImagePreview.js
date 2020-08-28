074a8a6df63e9cac503dbf46ccf79ac502ba3095
Theo Goss <theo@exorttech.co.uk>
2020-08-28 13:37:01 +0100

    completed Image Previewer so tht the button now works to trigger updated image.

--- react-questions-2/src/Components/ImagePreview.js
+++ react-questions-2/src/Components/ImagePreview.js
@@ -20,10 +20,13 @@ function ImagePreview () {
     
     const [image, setImage] = useState ('https://cdn.theatlantic.com/thumbor/pN25nhF1hatn7QpckNtABKwzmoI=/0x61:1000x624/720x405/media/old_wire/img/upload/2013/03/18/happydog/original.jpg')
     const setUserImage = (event) => setImage(event.target.value)
-    const submit = () => setUserImage(image)
+    
+    const [newImage, setNewImage] = useState (image)
+    const submit = () => setNewImage(image)
+
     return (
         <div>
-            <img src={image}></img> 
+            <img src={newImage}></img> 
             <Input imageSelect={setUserImage} />
             <button onClick={submit}>Preview Image</button>
         </div>
